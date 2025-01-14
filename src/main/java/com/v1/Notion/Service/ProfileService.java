ec00cdf9fd1166c0a76305e7494e9be8276820af
VARDHAN REDDY <vardhanreddy1411@gmail.com>
2025-01-14 22:50:57 +0530

    Completed Profile module till updated fully

--- src/main/java/com/v1/Notion/Service/ProfileService.java
+++ src/main/java/com/v1/Notion/Service/ProfileService.java
@@ -6,5 +6,5 @@ import com.v1.Notion.DTO.UpdateProfile;
 import com.v1.Notion.config.ApiResponse;
 
 public interface ProfileService {
-	public ApiResponse updateProfile(UpdateProfile updateProfile,Principal principal);
+	public ApiResponse updateProfile(UpdateProfile updateProfile,String email);
 }
--- src/main/java/com/v1/Notion/Service/ProfileServiceImpl.java
+++ src/main/java/com/v1/Notion/Service/ProfileServiceImpl.java
@@ -1,16 +1,52 @@
 package com.v1.Notion.Service;
 
 import java.security.Principal;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.v1.Notion.DTO.UpdateProfile;
+import com.v1.Notion.Model.Profile;
+import com.v1.Notion.Model.User;
+import com.v1.Notion.Repository.ProfileRepository;
+import com.v1.Notion.Repository.UserRepository;
 import com.v1.Notion.config.ApiResponse;
 
 public class ProfileServiceImpl implements ProfileService{
-
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private ProfileRepository profileRepository;
 	@Override
-	public ApiResponse updateProfile(UpdateProfile updateProfile,Principal principal) {
-		
-		return null;
+	public ApiResponse updateProfile(UpdateProfile updateProfile,String email) {
+		Optional<User> optionalUser = userRepository.findByEmail(email);
+		if (!optionalUser.isPresent()) {
+		    return new ApiResponse(false,"User not found");
+		}
+		User user = optionalUser.get();
+
+		if (updateProfile.getFirstName() != null) user.setFirstName(updateProfile.getFirstName());
+        if (updateProfile.getLastName() != null) user.setLastName(updateProfile.getLastName());
+        userRepository.save(user);
+        
+        Optional<Profile> optionalProfile = profileRepository.findById(user.getAdditionalDetails().getId());
+        if (!optionalProfile.isPresent()) {
+		    return new ApiResponse(false,"Profile not found");
+		}
+        Profile profile = optionalProfile.get();
+        if (updateProfile.getDateOfBirth() != null) profile.setDateOfBirth(updateProfile.getDateOfBirth());
+        if (updateProfile.getAbout() != null) profile.setAbout(updateProfile.getAbout());
+        if (updateProfile.getContactNumber() != null) {
+            profile.setContactNumber(Long.parseLong(updateProfile.getContactNumber()));
+        }
+        if (updateProfile.getGender() != null) profile.setGender(updateProfile.getGender());
+        profileRepository.save(profile);
+        Optional<User> optionalUsers = userRepository.findById(user.getId());
+        if (!optionalUsers.isPresent()) {
+        	return new ApiResponse(false,"User not found");
+        }
+        User updatedUser = optionalUser.get();
+        return new ApiResponse(true, "Profile updated successfully", updatedUser);
 	}
 
 }
